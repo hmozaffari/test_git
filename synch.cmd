ac40210f1f4f2036a519a58122d8fe22a4bfd751
Hooman Mozaffari <hmozaffari@users.noreply.github.com>
2017-02-10 11:19:39 -0500

    Update synch.cmd

--- synch.cmd
+++ synch.cmd
@@ -1,12 +1,18 @@
+git checkout hm2
+
+REM Show current status
 cls
 git status
 git branch -a
 
+REM Stage added files
 git add .
+REM Commit all changes
 git commit -a -m 'AutoSynch'
-
 
 git checkout master
 git pull origin master
 git merge hm2
 git push origin master
+git push origin hm2
+git checkout hm2
