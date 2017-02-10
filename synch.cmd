1ba98573955bff453a084e19b3fcce10cae68af9
Mozaffari, Hooman <hooman.mozaffari@bmo.com>
2017-02-10 09:44:35 -0500

    'AutoSynch'

--- synch.cmd
+++ synch.cmd
@@ -1,10 +1,12 @@
+REM Show current status
 cls
 git status
 git branch -a
 
+REM Stage added files
 git add .
+REM Commit all changes
 git commit -a -m 'AutoSynch'
-
 
 git checkout master
 git pull origin master
